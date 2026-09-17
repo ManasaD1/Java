5c045eb02096b4bd9bd4e17bb491cd785ae0b6a4
ManasaD1 <manasad052006@gmail.com>
2026-09-17 10:33:28 +0530

    multiples of a and b

---
+++ test3.java
@@ -0,0 +1,16 @@
+public class test3 {
+    public static void main(String[] args) {
+        int n=20;
+        int a=2;
+        int b=3;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%a==0||i%b==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+}
+}
+
+
