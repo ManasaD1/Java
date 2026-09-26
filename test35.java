c1e8914e1a9fa9ae3e459735b745daebcfd65bd7
ManasaD1 <manasad052006@gmail.com>
2026-09-26 10:08:13 +0530

    hash map

---
+++ test35.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class test35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (result.indexOf(ch) == -1) {
+                int count = 0;
+                for (int j = 0; j < s.length(); j++) {
+                    if (s.charAt(j) == ch) {
+                        count++;
+                    }
+                }
+                result = result + ch + count;
+            }
+        }
+        System.out.println(result);
+    }
+}
